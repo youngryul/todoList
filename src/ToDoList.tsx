021423026c5d2910bc60e825d7b3cc8004301735
Leeyoungryul <lekjkj@naver.com>
2024-10-27 23:50:19 +0900

    10/27 프로젝트 초기 생성

---
+++ src/ToDoList.tsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+
+function ToDoList() {
+    const [ toDo, setTodo ] = useState("");
+    const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+        const {currentTarget: {value}} =event;
+        setTodo(toDo);
+    };
+    const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+                <button>Add</button>
+            </form>
+        </div>
+    );
+}
+
+export default ToDoList;
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#2f3640",
+    textColor: "#f5f6fa",
+    accentColor: "#44bd32",
+    cardBgColor: "transparent",
+};
+
+export const lightTheme: DefaultTheme = {
+    bgColor: "whitesmoke",
+    textColor: "black",
+    accentColor: "#9c88ff",
+    cardBgColor: "white",
+};
